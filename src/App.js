1c36254d8dc36a1c8d01ff149145f41a4db8babd
Mohd Fazil <kfazil185@gmail.com>
2022-04-06 16:23:15 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState, useRef, useMemo, useCallback, useEffect } from 'react'
 import './App.css';
+import CheckM from './CheckM'
 
 function App() {
+  const [value, setValue] = useState(0)
+  const [count, setCount] = useState(0)
+  const [list, setList] = useState([])
+  const input = useRef()
+  const memoCom = useMemo(() =>
+    <CheckM />
+    , [count])
+
+  const getItems = useCallback(
+    () => {
+      console.log("useCallback")
+      return value === isNaN ? [value, parseInt(value) + 2, parseInt(value) + 3] : [0]
+    }, [value]
+  )
+  useEffect(() => {
+    setList(getItems())
+  }, [value])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={input} type='text' value={value} onChange={e => setValue(e.target.value)} />
+      <button onClick={() => {
+        input.current.style.backgroundColor = 'black';
+        setCount(count + 1)
+      }}>change color</button>
+      {list.map((i) => <h2 key={i}>{i}</h2>)}
+      <h4>{count}</h4>
+      <CheckM />
+      <br />
+      {memoCom}
+
     </div>
   );
 }
---
+++ src/CheckM.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CheckM() {
+	console.log("checkM")
+	return (
+		<div>Child Component for Memo </div>
+	)
+}
